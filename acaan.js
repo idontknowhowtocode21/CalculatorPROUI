2fc2d10105f8217ffc8b52648d0282fa925ee3f0
idontknowhowtocode21 <pprivatebrna43@gmail.com>
2026-04-04 13:58:42 +0530

    Update acaan.js

--- acaan.js
+++ acaan.js
@@ -4,7 +4,8 @@ const STACK = [
     "8H","6S","5S","9H","KC","2D","JH","3S","8S","6H","10C","5D","KD",
     "2C","3H","8D","5C","KS","JD","8C","10S","KH","JC","7S","10H","AD",
     "4S","7H","4D","AC","9C","JS","QD","7C","QS","10D","6C","AH","9D"
-];
+]; //
+
 
 let acaanActive = false;
 let cardVal = "";
